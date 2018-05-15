8c141f24d0e0025a1db4b580b2072e3eb5b1d5ff
kristen autumn blackburn <kblack37@uw.edu>
2018-05-14 20:27:08 -0700

    initial commit

---
+++ src/Main.hx
@@ -0,0 +1,19 @@
+package;
+
+import openfl.display.Sprite;
+import openfl.Lib;
+
+/**
+ * ...
+ * @author kristen autumn blackburn
+ */
+class Main extends Sprite {
+
+	public function new() {
+		super();
+		
+		// Assets:
+		// openfl.Assets.getBitmapData("img/assetname.jpg");
+	}
+
+}
